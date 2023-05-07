fce6d1a58626c5291068953a540d94233336ab20
Hordiyenko Tetyana <hordit@me.com>
2023-05-08 00:17:00 +0200

    fixe error from diploy

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field } from 'formik';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import { ButtonAdd, FormStyled, InputStyled, Label } from './ContactForm.styled';
